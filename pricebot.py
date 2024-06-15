03585bb39bb1227d163a9022cb896ee7b804871c
artturilehtinen <artturi.lehtinen11@gmail.com>
2024-06-15 13:08:12 +0300

    Added rudimentary menu

--- pricebot.py
+++ pricebot.py
@@ -1,27 +1,37 @@
 import os
 from dotenv import load_dotenv
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
+
+def create_reply_markup():
+    keyboard = [
+
+        [InlineKeyboardButton("Bitcoin", callback_data="btc")],
+
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    return reply_markup
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Olen botti, joka kertoo bitcoinin hinnan!"
-    )
+    await update.message.reply_text("I check crypto prices for you! Select currency:", reply_markup=create_reply_markup())
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+
+    await query.answer()
+
+    await query.edit_message_text(text=f"0€")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Select currency:", reply_markup=create_reply_markup())
 
 
 def main():
-    print("Hello world!")
     load_dotenv()
     key = os.getenv('TELEGRAM_API_KEY')
 
     app = ApplicationBuilder().token(key).build()
 
-    app.add_handler(CommandHandler("hello", hello))
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(CallbackQueryHandler(button))
     app.run_polling()
 
 if __name__ == "__main__":
