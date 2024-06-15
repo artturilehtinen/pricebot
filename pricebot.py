95adce7f6d83d6560aec0e3dae43d778786390ab
artturilehtinen <artturi.lehtinen11@gmail.com>
2024-06-15 11:28:12 +0300

    Barebones telegram bot

--- pricebot.py
+++ pricebot.py
@@ -1,5 +1,28 @@
+import os
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Olen botti, joka kertoo bitcoinin hinnan!"
+    )
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
 def main():
     print("Hello world!")
+    load_dotenv()
+    key = os.getenv('TELEGRAM_API_KEY')
+
+    app = ApplicationBuilder().token(key).build()
+
+    app.add_handler(CommandHandler("hello", hello))
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
